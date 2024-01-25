898b9166c9d8581ea442c082a9f97fed5a29de59
Alberto Rivas <alberto.rivas2264@alumnos.udg.mx>
2024-01-24 21:42:40 -0600

    Interfaz flask

--- SIC-pre-agua.py
+++ SIC-pre-agua.py
@@ -1,32 +1,37 @@
 from flask import Flask, render_template, redirect, flash, request, url_for
-from sensores import FlowControl, PresionResultados
+from sensores import FlowControl, PresionResultados, classify_presion
 
 
 app = Flask(__name__)
 flow_ctl = FlowControl()
 
+app.jinja_env.globals.update(classify_presion=classify_presion)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    names = flow_ctl.get_names()
+    values: list = []
+    for name in names:
+        values.append(flow_ctl.check_presion(name))
+    return render_template('index.html', values=zip(names, values))
 
 @app.route('/add-sensor', methods=["POST"])
 def add_sensor():
-    error: str = None
     setup_form = request.form
-    sensors_name = setup_form.get('name')
-    sensors_flow_start_gpio = setup_form.get('gpio_pin_start')
+    sensors_name = setup_form.get('set-name')
+    sensors_flow_start_gpio = int(setup_form.get('gpio_pin_start'))
     if setup_form.get('has_second_sensor'):
-        sensors_flow_end_gpio = setup_form.get('gpio_pin_end')
+        sensors_flow_end_gpio = int(setup_form.get('gpio_pin_end'))
     else:
         sensors_flow_end_gpio = None
 
     try:
         flow_ctl.add_sensor(sensors_name, sensors_flow_start_gpio, sensors_flow_end_gpio)
-        flash("Se ha agregado con exito los sensores")
+        flash("Se ha agregado con exito los sensores", "info")
     except Exception as e:
-        error = f"No se pudo configurar los sensores solicitados: {e}"
+        flash(f"No se pudo configurar los sensores solicitados: {e}", "error")
     finally:
-        return redirect('/', error=error)
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- sensores.py
+++ sensores.py
@@ -1,39 +1,19 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Tuple, Union
 import RPi.GPIO as GPIO
 import time
 import atexit
 
-'''
-class flow_setup():
-    def __init__(self, gpio_pin_flow: int, gpio_pin_flow_compare: int = None) -> None:
-        self.count: int = 0
-        self.start_counter: int = 0
-        self.flow: float = 0.0
-
-        # Atributos para el segundo comparador (Puede no necesitarlos)
-        self.count: int = 0
-        self.start_counter: int = 0
-        self.flow: float = 0.0
-        # Revisamos la cantidad de pins recibida
-        if gpio_pin_flow_compare:
-            # Si es mayor a uno, configuramos los dos
-                GPIO.setup(gpio_pin_flow, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-                GPIO.setup(gpio_pin_flow_compare, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-                # Tambien configuramos parametros extra para cada función
-        else:
-            # De otra forma, solo extraemos el primer numero
-            GPIO.setup(gpio_pin_flow, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        
-
-    def countPulse(self, channel):
-        if self.start_counter == 1:
-            self.count = self.count+1
-            
-    def countPulse_com(self, channel):
-        if self.start_counter == 1:
-            self.count = self.count+1
-'''
+def classify_presion(flow: int = 0) -> Union[float, str]:
+    # TODO: Los datos no son adecuados, se necesita datos reales
+    if flow == 0:
+        return "Sin presión"
+    elif flow < 30:
+        return "Presión baja"
+    elif 30 <= flow <= 30:
+        return "Presión media"
+    else:
+        return "Presión alta"
             
 class FlowSetup():
     def __init__(self, gpio_pin_flow: int) -> None:
@@ -59,24 +39,15 @@ class FlowSetup():
     
     def __clean_up(self) -> None:
         GPIO.cleanup(self.gpio_pin)
-
-    def __classify_presion(self, flow: int = 0) -> str:
-        # TODO: Los datos no son adecuados, se necesita datos reales
-        if flow < 2:
-            return "Presión baja"
-        elif 2 <= flow <= 5:
-            return "Presión media"
-        else:
-            return "Presión alta"
     
-    def get_presion(self) -> tuple(float,str):       
+    def get_presion(self) -> Tuple[float, str]:       
         self.start_counter = 1
         time.sleep(2)
         self.start_counter = 0
         flow = (self.count / 7.5)
         print("El flujo es: %.3f Litros/min" % (flow))
 
-        presion = self.__classify_presion(flow)
+        presion = classify_presion(flow)
         print("Estado de presión:", presion)
 
         self.count = 0
@@ -101,11 +72,11 @@ class FlowControl():
             self.sensores: dict = {}
     
     @atexit.register
-    def stop(self):
+    def stop(self) -> None:
         self.sensores.clear()
         GPIO.cleanup()
 
-    def add_sensor(self, name: str, gpio_pin: int, gpio_pin_compare: int = None):
+    def add_sensor(self, name: str, gpio_pin: int, gpio_pin_compare: int = None) -> None:
         try:
             if gpio_pin_compare:
                 self.sensores[name] = [FlowSetup(gpio_pin), FlowSetup(gpio_pin_compare)]
@@ -141,8 +112,8 @@ class FlowControl():
     def remove_sensors(self, name: str) -> None:
         self.sensores.pop(name)
 
-    def get_names(self) -> list(str):
-        return [ *self.sensores.keys() ] # [] crea la lista, * desempaca la lista como si introdujeramos los valores manualmente
+    def get_names(self) -> Tuple[str]:
+        return tuple( self.sensores.keys() ) # [] crea la lista, * desempaca la lista como si introdujeramos los valores manualmente
     
     def get_sensors(self, name: str):
         return self.sensores.get(name, None)
